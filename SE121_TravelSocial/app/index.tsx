916cadba3803336a4d62d5bc917c7cafb10a837e
thinh_nguyen_phuc <113797076+timothyagile@users.noreply.github.com>
2024-07-05 19:15:41 +0700

    navigator

--- SE121_TravelSocial/app/index.tsx
+++ SE121_TravelSocial/app/index.tsx
@@ -1,15 +1,63 @@
-import { Text, View } from "react-native";
+import {StyleSheet} from 'react-native'
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeScreen from "@/screen/HomeScreen";
+import TicketScreen from "@/screen/TicketScreen";
+import CollectionScreen from "@/screen/CollectionScreen";
+import ProfileScreen from "@/screen/ProfileScreen";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function Index() {
+const Tab = createBottomTabNavigator()
+
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+    <NavigationContainer independent={true}>
+      <Tab.Navigator 
+      screenOptions={{
+        tabBarStyle: styles.container,
+        tabBarItemStyle: styles.itemStyle,
+      }}>
+        <Tab.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{tabBarIcon: ({focused}) => <AntDesign name = "home" size={22}/>}}/>
+
+        <Tab.Screen 
+        name="Ticket" 
+        component={TicketScreen}
+        options={{tabBarIcon: ({focused}) => <AntDesign name = "home" size={22}/>}}/>
+
+        <Tab.Screen 
+        name="Collection" 
+        component={CollectionScreen}
+        options={{tabBarIcon: ({focused}) => <AntDesign name = "home" size={22}/>}}/>
+
+        <Tab.Screen 
+        name="Profile" 
+        component={ProfileScreen}
+        options={{tabBarIcon: ({focused}) => <AntDesign name = "profile" size={22}/>}}/>
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    height: "10%",
+    borderTopLeftRadius: 32,
+    borderTopRightRadius: 32,
+    shadowColor: "#000",
+  shadowOffset: {
+    width: 0,
+    height: 12,
+  },
+  shadowOpacity: 0.7,
+  shadowRadius: 16,
+  elevation: 24,
+  },
+
+  itemStyle: {
+    marginBottom: 5
+  }
+})
---
+++ SE121_TravelSocial/screen/ProfileScreen.tsx
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native'
+
+export default function HomeScreen ()
+{
+    return (
+        <View style = {{
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center"
+        }}>
+            <Text>Profile Screen</Text>
+        </View>
+    )
+}
