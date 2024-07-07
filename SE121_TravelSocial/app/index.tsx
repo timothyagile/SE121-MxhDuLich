f159ebe498bfdbf9033883fe0a1cddaba0355a16
1418299171 <22520573@gm.uit.edu.vn>
2024-07-07 19:25:15 +0700

    navigator

--- SE121_TravelSocial/app/index.tsx
+++ SE121_TravelSocial/app/index.tsx
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet, Image} from 'react-native'
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "@/screen/HomeScreen";
@@ -6,6 +6,7 @@ import TicketScreen from "@/screen/TicketScreen";
 import CollectionScreen from "@/screen/CollectionScreen";
 import ProfileScreen from "@/screen/ProfileScreen";
 import { AntDesign } from "@expo/vector-icons";
+import React from 'react';
 
 const Tab = createBottomTabNavigator()
 
@@ -20,22 +21,53 @@ export default function App() {
         <Tab.Screen 
         name="Home" 
         component={HomeScreen}
-        options={{tabBarIcon: ({focused}) => <AntDesign name = "home" size={22}/>}}/>
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <Image 
+              source={require('../assets/icons/iconoir_home-simple.png')} 
+              style={{ tintColor: focused ? "blue" : "gray", width: 22, height: 22 }} 
+            />
+          )
+        }}
+      />
 
         <Tab.Screen 
         name="Ticket" 
         component={TicketScreen}
-        options={{tabBarIcon: ({focused}) => <AntDesign name = "home" size={22}/>}}/>
-
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <Image 
+              source={require('../assets/icons/mingcute_ticket-line.png')} 
+              style={{ tintColor: focused ? "blue" : "gray", width: 22, height: 22 }} 
+            />
+          )
+        }}
+      />
         <Tab.Screen 
         name="Collection" 
         component={CollectionScreen}
-        options={{tabBarIcon: ({focused}) => <AntDesign name = "home" size={22}/>}}/>
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <Image 
+              source={require('../assets/icons/collection.png')} 
+              style={{ tintColor: focused ? "blue" : "gray", width: 22, height: 22 }} 
+            />
+          )
+        }}
+      />
 
         <Tab.Screen 
         name="Profile" 
         component={ProfileScreen}
-        options={{tabBarIcon: ({focused}) => <AntDesign name = "profile" size={22}/>}}/>
+        options={{ 
+          tabBarIcon: ({ focused }) => (
+            <Image 
+              source={require('../assets/icons/Profile.png')} 
+              style={{ tintColor: focused ? "blue" : "gray", width: 22, height: 22 }} 
+            />
+          )
+        }}
+      />      
       </Tab.Navigator>
     </NavigationContainer>
   )
