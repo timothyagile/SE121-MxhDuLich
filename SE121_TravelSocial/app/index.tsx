b562e4a2d293f424c9d821969fe4431203d03b31
thinh_nguyen_phuc <113797076+timothyagile@users.noreply.github.com>
2024-07-07 22:35:43 +0700

    onboarding
    not yet

--- SE121_TravelSocial/app/index.tsx
+++ SE121_TravelSocial/app/index.tsx
@@ -3,8 +3,6 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
 import WelcomeScreen1 from '@/screen/Welcome/WelcomeScreen1'
-import WelcomeScreen2 from '@/screen/Welcome/WelcomeScreen2'
-import WelcomeScreen3 from '@/screen/Welcome/WelcomeScreen3'
 import LoginScreen from '@/screen/Welcome/LoginScreen'
 import RegisterScreen from '@/screen/Welcome/RegisterScreen'
 import MainScreen from '@/screen/MainScreen'
@@ -19,20 +17,6 @@ export default function App() {
         <Stack.Screen 
         name = "welcome1" 
         component={WelcomeScreen1}
-        options={{
-          headerShown: false
-        }}/>
-
-        <Stack.Screen 
-        name = "welcome2" 
-        component={WelcomeScreen2}
-        options={{
-          headerShown: false
-        }}/>
-
-        <Stack.Screen 
-        name = "welcome3" 
-        component={WelcomeScreen3}
         options={{
           headerShown: false
         }}/>
---
+++ SE121_TravelSocial/constants/welcome-slides.tsx
@@ -0,0 +1,21 @@
+export default [
+    {
+        id: '1',
+        title: 'Plan your Luxurious Vacation',
+        description: '',
+        image: require('@/assets/images/welcome-screen1.png') 
+    },
+    {
+        id: '2',
+        title: 'Plan your Luxurious Vacation',
+        description: '',
+        image: require('@/assets/images/welcome-screen2.png') 
+    },
+    {
+        id: '3',
+        title: 'Plan your Luxurious Vacation',
+        description: '',
+        image: require('@/assets/images/welcome-screen3.png') 
+    },
+
+]
