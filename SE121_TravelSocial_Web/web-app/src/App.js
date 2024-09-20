5522720a0783f6d41d054993f69404e8685b4082
1418299171 <22520573@gm.uit.edu.vn>
2024-09-20 13:22:28 +0700

    create-web-for-admin

---
+++ SE121_TravelSocial_Web/web-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import '../src/styles/App.css';
+import LoginScreen from './pages/LoginScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginScreen/>
+    </div>
+  );
+}
+
+export default App;
