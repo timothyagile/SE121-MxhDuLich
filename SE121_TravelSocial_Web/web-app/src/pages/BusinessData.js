f51026effc793192ae09af7b22fa81777c453659
1418299171 <22520573@gm.uit.edu.vn>
2024-10-21 20:12:21 +0700

    fix-search

--- SE121_TravelSocial_Web/web-app/src/pages/BusinessData.js
+++ SE121_TravelSocial_Web/web-app/src/pages/BusinessData.js
@@ -4,16 +4,16 @@ export const businesses = [
   { id: 3, name: 'Quán ăn Sài Gòn', owner: 'Phạm QA', type: 'restaurant',status: 'đang chờ',code: 'KH003', contact: '0123456789', phone: '0977654321', avatar: 'avt.png',},
   { id: 4, name: 'Homestay Hà Nội', owner: 'Lê TH', type: 'homestay', status: 'đã duyệt',code: 'KH004', contact: '0989988776', phone: '0122334455', avatar: 'avt.png', },
   { id: 5, name: 'Khu du lịch Phan Thiết', owner: 'Võ MQ',type: 'resort', status: 'đang chờ', code: 'KH005', contact: '0777765432',phone: '0909091234', avatar: 'avt.png', },
-  { id: 1, name: 'Cắm trại hồ sóc - Vũng Tàu', owner: 'Nguyễn PT',type: 'camping',status: 'đang chờ', code: 'KH001', contact: '079303041265',  phone: '0987654321', avatar: 'avt.png',},
-  { id: 2,name: 'Khu nghỉ dưỡng Đà Lạt', owner: 'Trần QN', type: 'resort', status: 'đã duyệt', code: 'KH002',contact: '0981122334', phone: '0912345678', avatar: 'avt.png',},
-  { id: 3, name: 'Quán ăn Sài Gòn', owner: 'Phạm QA', type: 'restaurant',status: 'đang chờ',code: 'KH003', contact: '0123456789', phone: '0977654321', avatar: 'avt.png',},
-  { id: 4, name: 'Homestay Hà Nội', owner: 'Lê TH', type: 'homestay', status: 'đã duyệt',code: 'KH004', contact: '0989988776', phone: '0122334455', avatar: 'avt.png', },
-  { id: 5, name: 'Khu du lịch Phan Thiết', owner: 'Võ MQ',type: 'resort', status: 'đang chờ', code: 'KH005', contact: '0777765432',phone: '0909091234', avatar: 'avt.png', },
-  { id: 1, name: 'Cắm trại hồ sóc - Vũng Tàu', owner: 'Nguyễn PT',type: 'camping',status: 'đang chờ', code: 'KH001', contact: '079303041265',  phone: '0987654321', avatar: 'avt.png',},
-  { id: 2,name: 'Khu nghỉ dưỡng Đà Lạt', owner: 'Trần QN', type: 'resort', status: 'đã duyệt', code: 'KH002',contact: '0981122334', phone: '0912345678', avatar: 'avt.png',},
-  { id: 3, name: 'Quán ăn Sài Gòn', owner: 'Phạm QA', type: 'restaurant',status: 'đang chờ',code: 'KH003', contact: '0123456789', phone: '0977654321', avatar: 'avt.png',},
-  { id: 4, name: 'Homestay Hà Nội', owner: 'Lê TH', type: 'homestay', status: 'đã duyệt',code: 'KH004', contact: '0989988776', phone: '0122334455', avatar: 'avt.png', },
-  { id: 5, name: 'Khu du lịch Phan Thiết', owner: 'Võ MQ',type: 'resort', status: 'đang chờ', code: 'KH005', contact: '0777765432',phone: '0909091234', avatar: 'avt.png', }
+  { id: 6, name: 'Cắm trại hồ sóc - Vũng Tàu', owner: 'Nguyễn PT',type: 'camping',status: 'đang chờ', code: 'KH001', contact: '079303041265',  phone: '0987654321', avatar: 'avt.png',},
+  { id: 7,name: 'Khu nghỉ dưỡng Đà Lạt', owner: 'Trần QN', type: 'resort', status: 'đã duyệt', code: 'KH002',contact: '0981122334', phone: '0912345678', avatar: 'avt.png',},
+  { id: 8, name: 'Quán ăn Sài Gòn', owner: 'Phạm QA', type: 'restaurant',status: 'đang chờ',code: 'KH003', contact: '0123456789', phone: '0977654321', avatar: 'avt.png',},
+  { id: 9, name: 'Homestay Hà Nội', owner: 'Lê TH', type: 'homestay', status: 'đã duyệt',code: 'KH004', contact: '0989988776', phone: '0122334455', avatar: 'avt.png', },
+  { id: 10, name: 'Khu du lịch Phan Thiết', owner: 'Võ MQ',type: 'resort', status: 'đang chờ', code: 'KH005', contact: '0777765432',phone: '0909091234', avatar: 'avt.png', },
+  { id: 11, name: 'Cắm trại hồ sóc - Vũng Tàu', owner: 'Nguyễn PT',type: 'camping',status: 'đang chờ', code: 'KH001', contact: '079303041265',  phone: '0987654321', avatar: 'avt.png',},
+  { id: 12,name: 'Khu nghỉ dưỡng Đà Lạt', owner: 'Trần QN', type: 'resort', status: 'đã duyệt', code: 'KH002',contact: '0981122334', phone: '0912345678', avatar: 'avt.png',},
+  { id: 13, name: 'Quán ăn Sài Gòn', owner: 'Phạm QA', type: 'restaurant',status: 'đang chờ',code: 'KH003', contact: '0123456789', phone: '0977654321', avatar: 'avt.png',},
+  { id: 14, name: 'Homestay Hà Nội', owner: 'Lê TH', type: 'homestay', status: 'đã duyệt',code: 'KH004', contact: '0989988776', phone: '0122334455', avatar: 'avt.png', },
+  { id: 15, name: 'Khu du lịch Phan Thiết', owner: 'Võ MQ',type: 'resort', status: 'đang chờ', code: 'KH005', contact: '0777765432',phone: '0909091234', avatar: 'avt.png', }
 ];
 
 
