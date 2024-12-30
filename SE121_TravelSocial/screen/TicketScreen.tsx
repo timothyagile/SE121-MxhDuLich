4a926cd2d10aa74421eb8802a102c4558e1cc304
1418299171 <22520573@gm.uit.edu.vn>
2024-12-30 16:59:48 +0700

    done-feature-reviewreview

--- SE121_TravelSocial/screen/TicketScreen.tsx
+++ SE121_TravelSocial/screen/TicketScreen.tsx
@@ -9,6 +9,7 @@ import axios from 'axios';
 export default function TicketScreen() {
   const { userId } = useUser();
   const [tickets, setTickets] = useState<any[]>([]);
+  const [locationId, setLocationId] = useState('');
   const [loading, setLoading] = useState(false);
   const [refreshing, setRefreshing] = useState(false);
 
@@ -39,7 +40,7 @@ export default function TicketScreen() {
 
             // Gọi API location để lấy tên địa điểm
             const location = room && room.locationId ? await fetchLocationDetails(room.locationId) : null;
-
+            setLocationId(room.locationId);
             return {
               ...ticket,
               locationName: location ? location.name : 'Unknown Location', // Bổ sung tên địa điểm
@@ -223,6 +224,7 @@ export default function TicketScreen() {
                 imageUrl={item.imageUrl || 'https://via.placeholder.com/150'}
                 onCancel={() => console.log(`Cancel ticket: ${item._id}`)}
                 bookingId={item._id}
+                locationId={locationId}
                 />
             )}
             refreshControl={
