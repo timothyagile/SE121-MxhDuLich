d544aaf7906b0d46ee1a3fec54d117b95d10347f
thinh_nguyen_phuc <113797076+timothyagile@users.noreply.github.com>
2024-12-31 02:50:38 +0700

    vnpay

---
+++ SE121_TravelSocial-Backend/routes/vnpayRoute.js
@@ -0,0 +1,131 @@
+let express = require('express');
+let router = express.Router();
+let $ = require('jquery');
+const request = require('request');
+const moment = require('moment');
+
+
+router.get('/', function(req, res, next){
+    res.render('orderlist', { title: 'Danh sách đơn hàng' })
+});
+
+router.get('/create_payment_url', function (req, res, next) {
+    res.render('order', {title: 'Tạo mới đơn hàng', amount: 10000})
+});
+
+router.get('/querydr', function (req, res, next) {
+    
+    let desc = 'truy van ket qua thanh toan';
+    res.render('querydr', {title: 'Truy vấn kết quả thanh toán'})
+});
+
+router.get('/refund', function (req, res, next) {
+    
+    let desc = 'Hoan tien GD thanh toan';
+    res.render('refund', {title: 'Hoàn tiền giao dịch thanh toán'})
+});
+
+
+router.post('/create_payment_url', function (req, res, next) {
+    
+    process.env.TZ = 'Asia/Ho_Chi_Minh';
+    
+    let date = new Date();
+    let createDate = moment(date).format('YYYYMMDDHHmmss');
+    
+    let ipAddr = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+
+    let config = require('config');
+    
+    let tmnCode = config.get('vnp_TmnCode');
+    let secretKey = config.get('vnp_HashSecret');
+    let vnpUrl = config.get('vnp_Url');
+    let returnUrl = config.get('vnp_ReturnUrl');
+    let orderId = moment(date).format('DDHHmmss');
+    let amount = req.body.amount;
+    let bankCode = req.body.bankCode;
+    
+    let locale = req.body.language;
+    if(locale === null || locale === ''){
+        locale = 'vn';
+    }
+    let currCode = 'VND';
+    let vnp_Params = {};
+    vnp_Params['vnp_Version'] = '2.1.0';
+    vnp_Params['vnp_Command'] = 'pay';
+    vnp_Params['vnp_TmnCode'] = tmnCode;
+    vnp_Params['vnp_Locale'] = locale;
+    vnp_Params['vnp_CurrCode'] = currCode;
+    vnp_Params['vnp_TxnRef'] = orderId;
+    vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId;
+    vnp_Params['vnp_OrderType'] = 'other';
+    vnp_Params['vnp_Amount'] = amount * 100;
+    vnp_Params['vnp_ReturnUrl'] = returnUrl;
+    vnp_Params['vnp_IpAddr'] = ipAddr;
+    vnp_Params['vnp_CreateDate'] = createDate;
+    if(bankCode !== null && bankCode !== ''){
+        vnp_Params['vnp_BankCode'] = bankCode;
+    }
+
+    vnp_Params = sortObject(vnp_Params);
+
+    let querystring = require('qs');
+    let signData = querystring.stringify(vnp_Params, { encode: false });
+    let crypto = require("crypto");     
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex"); 
+    vnp_Params['vnp_SecureHash'] = signed;
+    vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false });
+
+    res.redirect(vnpUrl)
+});
+
+router.get('/vnpay_return', function (req, res, next) {
+    let vnp_Params = req.query;
+
+    let secureHash = vnp_Params['vnp_SecureHash'];
+
+    delete vnp_Params['vnp_SecureHash'];
+    delete vnp_Params['vnp_SecureHashType'];
+
+    vnp_Params = sortObject(vnp_Params);
+
+    let config = require('config');
+    let tmnCode = config.get('vnp_TmnCode');
+    let secretKey = config.get('vnp_HashSecret');
+
+    let querystring = require('qs');
+    let signData = querystring.stringify(vnp_Params, { encode: false });
+    let crypto = require("crypto");     
+    let hmac = crypto.createHmac("sha512", secretKey);
+    let signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+
+    if(secureHash === signed){
+        //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+
+        res.render('success', {code: vnp_Params['vnp_ResponseCode']})
+    } else{
+        res.render('success', {code: '97'})
+    }
+});
+
+function sortObject(obj) {
+	let sorted = {};
+	let str = [];
+	let key;
+	for (key in obj){
+		if (obj.hasOwnProperty(key)) {
+		str.push(encodeURIComponent(key));
+		}
+	}
+	str.sort();
+    for (key = 0; key < str.length; key++) {
+        sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    }
+    return sorted;
+}
+
+module.exports = router;
