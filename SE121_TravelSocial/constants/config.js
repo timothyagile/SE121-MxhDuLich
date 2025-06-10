3957cdbd79c5cb03dde224b0f6c855def90dce1a
1418299171 <22520573@gm.uit.edu.vn>
2025-06-10 18:58:44 +0700

    fix-performance-v2

--- SE121_TravelSocial/constants/config.js
+++ SE121_TravelSocial/constants/config.js
@@ -1,5 +1,5 @@
 
-export const API_BASE_URL = 'http://172.16.2.172:3000';
+export const API_BASE_URL = 'http://192.168.2.54:3000';
 export const API_RCM_URL = 'http://10.0.2.2:5000';
 
 // export const API_BASE_URL = 'http://10.10.99.88:3000';
